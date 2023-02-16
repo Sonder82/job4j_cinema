ad727ca1de4180183d4c21e7a63c070694a7750c
Sonder82 <Himrs82@gmail.com>
2023-02-16 17:54:15 +0500

    Add DB & properties. Add plugin Liquibase. Add files with posters. Add scripts ddl & dml for files and genres.

---
+++ db/scripts/002_dml_insert_files.sql
@@ -0,0 +1,6 @@
+insert into files(name, path) values
+('deadPool', 'files/deadPool.jpg'),
+('inception', 'files/inception.jpg'),
+('parents', 'files/parents.jpg'),
+('shawShenk_Redemption', 'files/shawShank_Redemption.jpg'),
+('zooTropolis', 'files/zooTropolis.jpg');
