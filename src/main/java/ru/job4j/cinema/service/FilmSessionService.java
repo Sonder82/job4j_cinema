225a7dae8f3dda2f93e6b09750477aa86853b480
Sonder82 <Himrs82@gmail.com>
2023-02-19 00:44:44 +0500

    Add Service Interfaces/update

--- src/main/java/ru/job4j/cinema/service/FilmSessionService.java
+++ src/main/java/ru/job4j/cinema/service/FilmSessionService.java
@@ -2,6 +2,7 @@ package ru.job4j.cinema.service;
 
 import ru.job4j.cinema.model.FilmSession;
 import java.util.Collection;
+
 import java.util.Optional;
 
 public interface FilmSessionService {
--- src/main/java/ru/job4j/cinema/service/TicketService.java
+++ src/main/java/ru/job4j/cinema/service/TicketService.java
@@ -1,7 +1,6 @@
 package ru.job4j.cinema.service;
 
 import ru.job4j.cinema.model.Ticket;
-
 import java.util.Optional;
 
 public interface TicketService {
