747ce6a71b68e8b1afb577f11608e05d87dae979
Sonder82 <Himrs82@gmail.com>
2023-02-22 15:51:53 +0500

    Add File & Film Controllers

--- src/main/java/ru/job4j/cinema/controller/FileController.java
+++ src/main/java/ru/job4j/cinema/controller/FileController.java
@@ -1,15 +1,18 @@
 package ru.job4j.cinema.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.job4j.cinema.service.FileService;
 
+/**
+ * Класс контроллер для работы с файлами
+ */
+
 @RestController
-@RequestMapping
+@RequestMapping("/files")
 public class FileController {
 
     private final FileService fileService;
@@ -18,6 +21,13 @@ public class FileController {
         this.fileService = fileService;
     }
 
+    /**
+     * Метод поиска файла по id.
+     * Если файл не найден, то клиенту возвращается статус 404
+     * Если файл найден, статус 200 с телом ответа в виде содержимого файла.
+     * @param id id
+     * @return вариация ответа
+     */
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable int id) {
         var contentOptional = fileService.getFileById(id);
--- src/main/java/ru/job4j/cinema/controller/FilmController.java
+++ src/main/java/ru/job4j/cinema/controller/FilmController.java
@@ -1,25 +1,37 @@
 package ru.job4j.cinema.controller;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.job4j.cinema.service.FilmService;
 
+/**
+ * Клаcc контроллер для работы с фильмами
+ */
+@ThreadSafe
 @Controller
 @RequestMapping("/films")
 public class FilmController {
 
     private final FilmService filmService;
 
+
     public FilmController(FilmService filmService) {
         this.filmService = filmService;
     }
 
+    /**
+     * Метод используется для вывода информации о всех фильмах
+     * @param model {@link Model}
+     * @return отображение информации о всех фильмах
+     */
     @GetMapping
     public String getAll(Model model) {
         model.addAttribute("films", filmService.findAll());
         return "films/list";
     }
-
 }
+
