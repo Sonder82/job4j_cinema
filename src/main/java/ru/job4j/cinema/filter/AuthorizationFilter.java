ae604651d3b148511e4da3b62458a80748e69eb0
Sonder82 <Himrs82@gmail.com>
2023-02-26 21:39:42 +0500

    Add filter

---
+++ src/main/java/ru/job4j/cinema/filter/AuthorizationFilter.java
@@ -0,0 +1,38 @@
+package ru.job4j.cinema.filter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class AuthorizationFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response,
+                            FilterChain chain) throws IOException, ServletException {
+        String uri = request.getRequestURI();
+        if (!notPermitted(uri)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        var userLoggedIn = request.getSession().getAttribute("user") != null;
+        if (!userLoggedIn) {
+            String loginPageUrl = request.getContextPath() + "/users/login";
+            response.sendRedirect(loginPageUrl);
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+
+    private boolean notPermitted(String uri) {
+        return uri.startsWith("/tickets/create");
+
+    }
+}
