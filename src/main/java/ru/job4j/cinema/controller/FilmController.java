78051f776c5b679be4e5282274a7a5b8b25397f9
Sonder82 <Himrs82@gmail.com>
2023-02-26 14:03:51 +0500

    Update Film view

--- src/main/java/ru/job4j/cinema/controller/FilmController.java
+++ src/main/java/ru/job4j/cinema/controller/FilmController.java
@@ -4,6 +4,7 @@ import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.job4j.cinema.service.FilmService;
 
@@ -31,6 +32,17 @@ public class FilmController {
     public String getAll(Model model) {
         model.addAttribute("films", filmService.findAll());
         return "films/list";
+    }
+
+    @GetMapping("/{id}")
+    public String getInfoFilmPage(Model model, @PathVariable int id) {
+        var filmOptional = filmService.findById(id);
+        if (filmOptional.isEmpty()) {
+            model.addAttribute("message", "Фильм не найден");
+            return "errors/404";
+        }
+        model.addAttribute("film", filmOptional.get());
+        return "films/info";
     }
 }
 
